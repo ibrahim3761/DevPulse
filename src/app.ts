531bedfae6b48c83f24dba00d4ba5a3ccbce8fd2
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-05-21 15:01:30 +0600

    issues-router-connected

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { type Application } from "express"
 import { authRoute } from "./module/auth/auth.route"
+import { issuesRouter } from "./module/issues/issues.route"
 
 const app : Application = express()
 
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
+app.use('api/issues', issuesRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
